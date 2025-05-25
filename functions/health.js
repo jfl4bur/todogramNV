84ecc466f51d06bc2a9ca3491e17f69b8672f378
jfl4bur <74684004+jfl4bur@users.noreply.github.com>
2025-05-25 11:56:47 +0200

    Delete functions directory

--- functions/health.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ status: 'ok' }),
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  };
-};
